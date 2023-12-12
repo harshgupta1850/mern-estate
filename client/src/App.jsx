928294e0fe50ab726d581cd903af9e4018c1c87b
harshgupta1850 <t_Harsh.Gupta1@tatadigital.com>
2023-12-12 07:51:08 +0530

    create pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/home";
+import SignIn from "./pages/signIn";
+import SignUp from "./pages/signOut";
+import Profile from "./pages/profile";
+import About from "./pages/about";
 
-function Home() {
-    return <h1 className="text-red-500">Home hi</h1>;
+function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/sign-in" element={<SignIn />} />
+                <Route path="/sign-up" element={<SignUp />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/about" element={<About />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
-export default Home;
+export default App;
