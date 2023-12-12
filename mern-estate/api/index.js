36b327442697ad76b8e3d98a08ca4ec02c9e6188
harshgupta1850 <t_Harsh.Gupta1@tatadigital.com>
2023-12-12 15:05:35 +0530

    connect to database

--- mern-estate/api/index.js
+++ mern-estate/api/index.js
@@ -1,4 +1,12 @@
 import express from "express"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+dotenv.config()
+
+mongoose.connect(process.env.MONGO).then(() => {
+  console.log("connected successfukly")
+}).catch((error) => console.log(error, 'error'))
+
 const app = express()
 app.listen(1000, () => {
   console.log("server is running")
