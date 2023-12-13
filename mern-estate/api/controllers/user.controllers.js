8a87e2390a6ac4226016c245a6b92632ca0a8557
harshgupta1850 <t_Harsh.Gupta1@tatadigital.com>
2023-12-13 06:55:26 +0530

    test api added

---
+++ mern-estate/api/controllers/user.controllers.js
@@ -0,0 +1,7 @@
+const test = (req, res) => {
+  res.json({
+    message: 'helloo world'
+  })
+}
+
+export default test
--- mern-estate/api/index.js
+++ mern-estate/api/index.js
@@ -1,13 +1,21 @@
 import express from "express"
 import mongoose from "mongoose"
 import dotenv from "dotenv"
+import userRoutes from "./routes/user.routes.js"
+
 dotenv.config()
 
-mongoose.connect(process.env.MONGO).then(() => {
-  console.log("connected successfukly")
-}).catch((error) => console.log(error, 'error'))
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("connected successfukly")
+  })
+  .catch((error) => console.log(error, "error"))
 
 const app = express()
+
+app.use('/api/user', userRoutes)
+
 app.listen(1000, () => {
   console.log("server is running")
 })
