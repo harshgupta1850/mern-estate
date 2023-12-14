cce049148d415b3ae537607886380aa720e1ddb0
harshgupta1850 <t_Harsh.Gupta1@tatadigital.com>
2023-12-14 12:54:31 +0530

    Redux toolkit added

--- mern-estate/client/src/pages/signIn.jsx
+++ mern-estate/client/src/pages/signIn.jsx
@@ -1,38 +1,46 @@
 import React, { useState } from "react";
-import { Link,useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import {
+    signInFailure,
+    signInStart,
+    signInSuccess,
+} from "../redux/user/userSlice";
 
 function Signin() {
-    const navigate = useNavigate()
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
     const [formData, setFormData] = useState({});
-    const [error, setError] = useState(null);
-    const [loading, setLoading] = useState(false);
+    const { error, loading, currentUser } = useSelector((state) => state.user);
+    console.log(currentUser);
     const handleChange = (e) => {
         setFormData({
             ...formData,
             [e.target.id]: e.target.value,
         });
     };
-
     const handleSumbit = async (e) => {
         e.preventDefault();
-        setLoading(true);
-        setError(false);
-        const res = await fetch("/api/auth/signin", {
-            method: "post",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(formData),
-        });
-        const data = await res.json();
-        if (data.success === false) {
-            setLoading(false);
-            setError(data.message);
-            return;
+        try {
+            dispatch(signInStart());
+            const res = await fetch("/api/auth/signin", {
+                method: "post",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(formData),
+            });
+            const data = await res.json();
+            if (data.success === false) {
+                dispatch(signInFailure(data.message));
+                return;
+            }
+            dispatch(signInSuccess(data));
+            navigate("/");
+            console.log(data);
+        } catch (error) {
+            dispatch(signInFailure(error.message));
         }
-        setLoading(false);
-        navigate('/')
-        console.log(data);
     };
 
     console.log(formData);
---
+++ mern-estate/client/src/redux/user/userSlice.js
@@ -0,0 +1,31 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = {
+  loading: false,
+  error: null,
+  currentUser: null
+}
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    signInStart: (state) => {
+      state.error = null
+      state.loading = true
+    },
+    signInSuccess: (state, action) => {
+      state.currentUser = action.payload
+      state.loading = false
+      state.error = null
+    },
+    signInFailure: (state) => {
+      state.currentUser = null
+      state.loading = false
+      state.error = action.payload
+    }
+  }
+})
+
+export const { signInFailure, signInStart, signInSuccess } = userSlice.actions
+export default userSlice.reducer
