04d73d99fa6fbf59be6974ecae3ef0863e82cdcc
harshgupta1850 <t_Harsh.Gupta1@tatadigital.com>
2023-12-14 07:34:48 +0530

    sign in api route added

--- mern-estate/api/controllers/auth.controller.js
+++ mern-estate/api/controllers/auth.controller.js
@@ -1,6 +1,9 @@
 import User from "../models/user.model.js"
 import bcryptjs from "bcryptjs"
-const signup = async (req, res, next) => {
+import errorHandler from "../utils/error.js"
+import jwt from "jsonwebtoken"
+
+export const signup = async (req, res, next) => {
   console.log(req.body)
   const { username, email, password } = req.body
   const hashedPassword = bcryptjs.hashSync(password, 10)
@@ -12,7 +15,20 @@ const signup = async (req, res, next) => {
     console.log(error)
     next(error)
   }
-
 }
 
-export default signup
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body
+
+  try {
+    const validUser = await User.findOne({ email })
+    if (!validUser) return next(errorHandler(405, 'User Not Found'))
+    const validatePassword = bcryptjs.compareSync(password, validUser.password)
+    if (!validatePassword) return next(errorHandler(406, 'Wrong credential'))
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET)
+    const { password: pass, ...rest } = validUser._doc
+    res.cookie('access_token', token, { httpOnly: true }).status(200).json(rest)
+  } catch (error) {
+    next(error)
+  }
+}
