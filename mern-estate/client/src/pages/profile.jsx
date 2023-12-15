6a1ebf36f675ffb65a878d28dde86d6c7f4809a4
harshgupta1850 <t_Harsh.Gupta1@tatadigital.com>
2023-12-15 10:55:09 +0530

    profikle page ui

--- mern-estate/client/src/pages/profile.jsx
+++ mern-estate/client/src/pages/profile.jsx
@@ -1,7 +1,46 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 function Profile() {
-    return <h1>Profile</h1>;
+    const currentUser = useSelector((state) => state.user.currentUser);
+    return (
+        <div className="p-3 max-w-lg mx-auto">
+            <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
+            <form className="flex flex-col gap-4">
+                <img
+                    src={currentUser.avatar}
+                    className="rounded-full object-cover h-24 w-24 cursor-pointer self-center mt-2"
+                />
+                <input
+                    type="text"
+                    className="border p-3 rounded-lg"
+                    placeholder="User name"
+                    id="username"
+                />
+                <input
+                    type="email"
+                    className="border p-3 rounded-lg"
+                    placeholder="Email"
+                    id="email"
+                />
+                <input
+                    type="password"
+                    className="border p-3 rounded-lg"
+                    placeholder="Password"
+                    id="password"
+                />
+                <button className="bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:80 p-3">
+                    update
+                </button>
+            </form>
+            <div className="flex justify-between mt-5">
+                <span className="text-red-700 cursor-pointer">
+                    Delete Account
+                </span>
+                <span className="text-red-700 cursor-pointer">Sign Out</span>
+            </div>
+        </div>
+    );
 }
 
 export default Profile;
