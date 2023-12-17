979b4cd48aa7fc416d64fcb4fd3e3f84d1c794dc
harshgupta1850 <t_Harsh.Gupta1@tatadigital.com>
2023-12-17 11:33:57 +0530

    fetch user listing

--- mern-estate/api/controllers/user.controller.js
+++ mern-estate/api/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js"
 import errorHandler from "../utils/error.js"
 import bcryptjs from "bcryptjs"
+import Listing from "../models/listing.model.js"
 
 export const test = (req, res) => {
   res.json({
@@ -39,4 +40,18 @@ export const deleteUser = async (req, res, next) => {
   } catch (error) {
     next(error)
   }
-} 
+}
+
+export const getUserListing = async (req, res, next) => {
+  if (req.user.id === req.params.id) {
+    try {
+      const listing = await Listing.find({userRef:req.params.id})
+      res.status(200).json(listing)
+    } catch (error) {
+      next(error)
+    }
+  } else {
+    next(errorHandler(401, "you can see only you listing"))
+  }
+
+}
