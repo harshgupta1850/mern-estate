f5869b612b1ba6e980be336e3ace5143d808b8e6
harshgupta1850 <t_Harsh.Gupta1@tatadigital.com>
2023-12-23 06:53:57 +0530

    search api routed created

--- mern-estate/api/routes/listing.router.js
+++ mern-estate/api/routes/listing.router.js
@@ -1,13 +1,13 @@
 import express from "express"
-import { createListing, deleteListing ,updateListing,getListingDetails} from "../controllers/listing.controller.js"
+import { createListing, deleteListing, updateListing, getListingDetails, getListings } from "../controllers/listing.controller.js"
 import { verifyUser } from "../utils/verifyUser.js"
 
 const router = express.Router()
 
 router.post('/create', verifyUser, createListing)
 router.delete('/delete/:id', verifyUser, deleteListing)
-router.post("/update/:id",verifyUser,updateListing)
-router.get("/get/:id",getListingDetails)
-
+router.post("/update/:id", verifyUser, updateListing)
+router.get("/get/:id", getListingDetails)
+router.get("/getListing", getListings)
 
 export default router
